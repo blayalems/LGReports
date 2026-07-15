d5f6a35f15d4de15eddd692980f95ad4f073f08b
Lemuel Blaya <blayalems@gmail.com>
2026-07-15 23:55:00 +0800

    Gate Pages with browser tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   reporter: [['list'], ['html', { open: 'never' }]],
   use: {
     baseURL: 'http://127.0.0.1:4173/LGReports/',
-    channel: 'chrome',
+    channel: process.env.CI ? undefined : 'chrome',
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
   },
